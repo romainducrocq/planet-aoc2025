4cbaf5cec27fd45786590f26eb2116d447f20e85
romainducrocq <romain.g.ducrocq@gmail.com>
2025-12-03 08:22:28 +0200

    build aoc

--- run_aoc.sh
+++ run_aoc.sh
@@ -2,14 +2,20 @@
 
 function run_day () {
     DAY="${1}"
+    if [ ${1} -lt 10 ]; then
+        DAY="0${DAY}"
+    fi
     echo ""
     echo "----- Day ${DAY} -----"
-    planet -O3 day0${DAY}.plx libaoc.plx
+    DAY="day${DAY}"
+
+    planet -O3 -o build/${DAY} ${DAY}.plx libaoc.plx
     if [ ${?} -ne 0 ]; then exit 1; fi
-    ./day0${DAY}
+    ./build/${DAY}
     if [ ${?} -ne 0 ]; then exit 1; fi
 }
 
+mkdir -p build/
 if [ ! -z "${1}" ]; then
     run_day ${1}
 else
